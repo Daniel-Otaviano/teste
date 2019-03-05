8bc7f32468c932ca3b15e914ff376c3e2f93e807
Daniel-Otaviano <daniel.santos697@gmail.com>
2019-03-05 14:15:19 -0300

    segundo

---
+++ EX009.py
@@ -0,0 +1,21 @@
+#Exercícío de Tabuada
+
+n = int(input('Digite um número para ver a sua tabuada: '))
+print('{:-^20}'.format('Tabuada'))
+print('{} x {:2} = {}'.format(n, 1, n*1))
+print('{} x {:2} = {}'.format(n, 2, n*1))
+print('{} x {:2} = {}'.format(n, 3, n*1))
+print('{} x {:2} = {}'.format(n, 4, n*1))
+print('{} x {:2} = {}'.format(n, 5, n*1))
+print('{} x {:2} = {}'.format(n, 6, n*1))
+print('{} x {:2} = {}'.format(n, 7, n*1))
+print('{} x {:2} = {}'.format(n, 8, n*1))
+print('{} x {:2} = {}'.format(n, 9, n*1))
+print('{} x {:2} = {}'.format(n, 10, n*1))
+print('-' * 20)
+
+#Para centralizar eu posso usar o .center
+#Posso utilizar o .format para colocar alguns sinais, exemplo {:*^20}
+#Para dar espacço entre os número posso coloca ' {:2} ' ou mais
+
+
